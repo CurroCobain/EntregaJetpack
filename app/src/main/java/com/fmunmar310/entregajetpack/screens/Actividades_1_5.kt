9d41144ed379853e95f77ddd18ebbd7b848435ab
GaelCobain <fmunmar310@g.educaand.es>
2023-11-09 19:25:11 +0100

    completo

--- app/src/main/java/com/fmunmar310/entregajetpack/screens/Actividades_1_5.kt
+++ app/src/main/java/com/fmunmar310/entregajetpack/screens/Actividades_1_5.kt
@@ -1,7 +1,9 @@
 package com.fmunmar310.entregajetpack.screens
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -23,6 +25,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+import kotlinx.coroutines.selects.whileSelect
 
 /*
 Actividad 1:
@@ -33,6 +37,7 @@ cuando se muestre la línea de progreso... Cuando pulsemos "Cancelar" vuelve al 
 @Composable
 fun Actividad1() {
     var showLoading by rememberSaveable { mutableStateOf(false) }
+    var texto by rememberSaveable { mutableStateOf("Cargar perfil") }
 
     Column(
         Modifier
@@ -43,6 +48,7 @@ fun Actividad1() {
     ) {
 
         if (showLoading) {
+            texto = "Cancelar"
             CircularProgressIndicator(
                 color = Color.Red,
                 strokeWidth = 10.dp
@@ -52,12 +58,15 @@ fun Actividad1() {
                 color = Color.Red,
                 trackColor = Color.LightGray
             )
+        }else {
+            texto = "Cargar perfil"
         }
 
         Button(
-            onClick = { showLoading = !showLoading }
+            onClick = { showLoading = !showLoading
+            }
         ) {
-            Text(text = "Cargar perfil")
+            Text(text = texto )
         }
     }
 }
@@ -68,6 +77,48 @@ Modifica ahora también que se separe el botón de la línea de progreso 30 dp,
 pero usando un estado... es decir, cuando no sea visible no quiero que tenga la separación
 aunque no se vea.
 */
+@Preview(showBackground = true)
+@Composable
+fun Actividad2() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+    var texto by rememberSaveable { mutableStateOf("Cargar perfil")}
+    var separacion by rememberSaveable { mutableStateOf( 0) }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (showLoading) {
+            texto = "Cancelar"
+            separacion = 30
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 10.dp
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+        }else {
+            texto = "Cargar Perfil"
+            separacion = 0
+        }
+
+        Button(
+            modifier = Modifier.padding(top = separacion.dp),
+            onClick = { showLoading = !showLoading
+
+            }
+        ) {
+            Text(text = texto )
+        }
+    }
+}
 
 
 /*
@@ -77,20 +128,29 @@ Actividad 3:
   cuando se pulse el botón Decrementar.
 - Evitar que nos pasemos de los márgenes de su propiedad progressStatus (0-1)
 */
+@Preview(showBackground = true)
 @Composable
 fun Actividad3() {
+    var progress by rememberSaveable { mutableStateOf(0f) }
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LinearProgressIndicator(progress = 0f)
+        LinearProgressIndicator(progress = progress)
 
-        Row(Modifier.fillMaxWidth()) {
-            Button(onClick = { /* TODO */ }) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp),
+            horizontalArrangement = Arrangement.Center) {
+            Button(onClick = { progress  = if(progress in 0f..1f) progress + 0.1f else progress}) {
                 Text(text = "Incrementar")
             }
-            Button(onClick = { /* TODO */ }) {
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Button(onClick = { progress = if(progress in 0.1f..1.1f) progress - 0.1f else progress}) {
                 Text(text = "Reducir")
             }
         }
@@ -104,15 +164,27 @@ Sitúa el TextField en el centro de la pantalla y haz que reemplace el valor de 
 y que no deje escribir más de un punto decimal...
 */
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun Actividad4() {
     var myVal by rememberSaveable { mutableStateOf("") }
+    Box( modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        TextField(
+            value = myVal,
+            onValueChange = { var text = it
+                if(text.contains(',')){
+                    text = text.replace(',','.')
+                }
+                if(text.count{c:Char -> c == '.'} <= 1){
+                    myVal= text
+                }
+                },
+            label = { Text(text = "Importe") }
+        )
+    }
 
-    TextField(
-        value = myVal,
-        onValueChange = { myVal = it },
-        label = { Text(text = "Importe") }
-    )
 }
 
 
@@ -123,14 +195,38 @@ al que debes añadir un padding alrededor de 15 dp y establecer colores diferent
 cuando tenga el foco y no lo tenga.
 A nivel funcional no permitas que se introduzcan caracteres que invaliden un número decimal.
 */
+@Preview
+@Composable
+fun PrevAvtividad5(){
+    var value by rememberSaveable { mutableStateOf("") }
+    Actividad5( value, onValueChange = {newValue ->
+        var text = newValue
+        if (text.contains(',')) {
+            text = text.replace(',', '.')
+        }
+        if (text.count { c: Char -> c == '.' } <= 1 && text.replace(".","").isDigitsOnly()){
+            value = text
+        }
+    })
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Actividad5() {
-    var myVal by rememberSaveable { mutableStateOf("") }
-
+fun Actividad5(string : String,
+               onValueChange : (String) -> Unit ) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
     OutlinedTextField(
-        value = myVal,
-        onValueChange = { myVal = it },
-        label = { Text(text = "Importe") }
+        value = string,
+        onValueChange = { onValueChange(it) },
+        label = { Text(text = "Importe") },
+        modifier = Modifier.padding(15.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Red,
+            unfocusedBorderColor = Color.Blue
+        )
     )
 }
+}
+
